5f7d82b1c5571efebe34a92097c516a7acf1997d
user.name like: "Naicon Martins <user.email like: "naiconmartins@hotmail.com>
2025-10-12 19:46:53 -0400

    Configurado projeto, layout e loading. Criado componente header

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,15 @@
+import { Header } from '@/components/header'
+import React from 'react'
+
+export default function StoreLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
+      <Header />
+      {children}
+    </div>
+  )
+}
